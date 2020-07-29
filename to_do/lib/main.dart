d86414a1dde8b0243b5706b0968f81e98589de80
steph-en-m <mstevejean@gmail.com>
2020-07-29 13:56:05 +0300

    to_do app layout

--- to_do/lib/main.dart
+++ to_do/lib/main.dart
@@ -12,13 +12,31 @@ class ToDo extends StatelessWidget{
       appBar: AppBar(
         title: Text('To-do list'),
         centerTitle: true,
-        backgroundColor: Colors.grey[840],
-        elevation: 0.0,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0, //remove bar drop-shadow
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
-          children: <Widget>[],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'TASK',
+              style: TextStyle(
+                color: Colors.grey[400],
+                fontSize: 18.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 25.0,),
+            TextField(
+              
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+              )
+            ),
+          ],
         ),
       ),
     )

4285cb53fd52b566366b9a664174895e3272ef2d
steph-en-m <mstevejean@gmail.com>
2020-07-29 13:28:56 +0300

    to_do app layout

--- to_do/lib/main.dart
+++ to_do/lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'To_do list app',
-    home: Center(child: Text('Add and remove tasks to/from your to-do list.')),
-  ));
+void main() => runApp(MaterialApp(
+      home: ToDo(),
+    ));
+
+class ToDo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('To-do list'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[840],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    )
+  }
 }

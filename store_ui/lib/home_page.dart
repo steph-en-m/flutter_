670452ffdbaae8b0e6465071d487271303f2c4ff
steph-en-m <mstevejean@gmail.com>
2020-08-05 22:35:24 +0300

    format layout

--- store_ui/lib/home_page.dart
+++ store_ui/lib/home_page.dart
@@ -1,125 +1,328 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
-//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() {
-  runApp(HomePage());
-}
+void main() => runApp(MyApp());
 
-class HomePage extends StatefulWidget {
-  final Widget child;
-
-  HomePage({Key key, this.child}) : super(key: key);
-
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<charts.Series<Task, String>> _pieSeries;
-  _generateData() {
-    var pieData = [
-      new Task('T-shirts', 23.0, Colors.amberAccent[700]),
-      new Task('Trousers', 12.0, Colors.teal[800]),
-      new Task('TV-sets', 55.0, Colors.green[200]),
-      new Task('Jeans', 43.0, Colors.blue[700]),
-      new Task('Shoes', 33.0, Colors.purple[300]),
-      new Task('Bags', 6.0, Colors.grey),
-    ];
-
-    _pieSeries.add(charts.Series(
-      data: pieData,
-      domainFn: (Task task, _) => task.task,
-      measureFn: (Task task, _) => task.taskvalue,
-      colorFn: (Task, task, _) => charts.ColorUtil.fromDartColor(task.colorval),
-      id: 'Monthly Sales',
-      labelAccessorFn: (Task row, _) => '${row.taskvalue}',
-    ));
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Awesome Store',
+      theme: ThemeData(
+        primaryColor: Color(0xffff6101),
+      ),
+      home: MyHomePage(title: 'Sales Dashboard 2020'),
+    );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  void initState() {
-    super.initState();
-    _generateData();
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var data = [0.0, 1.0, 1.5, 2.0, 0.0, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0];
+  var data1 = [0.0, -2.0, 3.5, -2.0, 0.5, 0.7, 0.8, 1.0, 2.0, 3.0, 3.2];
+
+  List<CircularStackEntry> circularData = <CircularStackEntry>[
+    new CircularStackEntry(
+      <CircularSegmentEntry>[
+        new CircularSegmentEntry(700.0, Color(0xff4285F4), rankKey: 'Q1'),
+        new CircularSegmentEntry(1000.0, Color(0xfff3af00), rankKey: 'Q2'),
+        new CircularSegmentEntry(1800.0, Color(0xffec3337), rankKey: 'Q3'),
+        new CircularSegmentEntry(1000.0, Color(0xff40b24b), rankKey: 'Q4'),
+      ],
+      rankKey: 'Quarterly Profits',
+    ),
+  ];
+
+  Material myTextItems(String title, String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material myCircularItems(String title, String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: AnimatedCircularChart(
+                      size: const Size(100.0, 100.0),
+                      initialChartData: circularData,
+                      chartType: CircularChartType.Pie,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material mychart1Items(String title, String priceVal, String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      priceVal,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: new Sparkline(
+                      data: data,
+                      lineColor: Color(0xffff6101),
+                      pointsMode: PointsMode.all,
+                      pointSize: 8.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material mychart2Items(String title, String priceVal, String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      priceVal,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: new Sparkline(
+                      data: data1,
+                      fillMode: FillMode.below,
+                      fillGradient: new LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.amber[800], Colors.amber[200]],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green[800],
-          bottom: TabBar(
-            indicatorColor: Colors.deepPurple[600],
-            tabs: [
-              Tab(icon: Icon(Icons.home)),
-              Tab(icon: Icon(Icons.menu)),
-              Tab(icon: Icon(Icons.shopping_basket))
-            ],
-          ),
-          title: Text('Awesome Store'),
-        ),
-        body: TabBarView(
-          children: [
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              //
+            }),
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(FontAwesomeIcons.chartLine),
+              onPressed: () {
+                //
+              }),
+        ],
+      ),
+      body: Container(
+        color: Color(0xffE5E5E5),
+        child: StaggeredGridView.count(
+          crossAxisCount: 4,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+          children: <Widget>[
             Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Container(
-                child: Center(
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        'Stock Inventory',
-                        style: TextStyle(
-                            fontSize: 24.0, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Expanded(
-                        child: charts.PieChart(_pieSeries,
-                            animate: true,
-                            animationDuration: Duration(seconds: 5),
-                            behaviors: [
-                              new charts.DatumLegend(
-                                outsideJustification:
-                                    charts.OutsideJustification.endDrawArea,
-                                horizontalFirst: false,
-                                desiredMaxRows: 2,
-                                cellPadding: new EdgeInsets.only(
-                                    right: 4.0, bottom: 4.0),
-                                entryTextStyle: charts.TextStyleSpec(
-                                    color: charts
-                                        .MaterialPalette.purple.shadeDefault,
-                                    fontFamily: 'Georgia',
-                                    fontSize: 11),
-                              )
-                            ],
-                            defaultRenderer: new charts.ArcRendererConfig(
-                                arcWidth: 100,
-                                arcRendererDecorators: [
-                                  new charts.ArcLabelDecorator(
-                                      labelPosition:
-                                          charts.ArcLabelPosition.inside)
-                                ])),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
+              padding: const EdgeInsets.all(8.0),
+              child:
+                  mychart1Items("Sales by Month", "421.3M", "+12.9% of target"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: myCircularItems("Quarterly Profits", "68.7M"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: myTextItems("Mktg. Spend", "48.6M"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: myTextItems("Users", "25.5M"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: mychart2Items("Conversion", "0.9M", "+19% of target"),
+            ),
+          ],
+          staggeredTiles: [
+            StaggeredTile.extent(4, 250.0),
+            StaggeredTile.extent(2, 250.0),
+            StaggeredTile.extent(2, 120.0),
+            StaggeredTile.extent(2, 120.0),
+            StaggeredTile.extent(4, 250.0),
           ],
         ),
       ),
-    ));
+    );
   }
-}
-
-// Datasets
-class Task {
-  String task;
-  double taskvalue;
-  Color colorval;
-
-  Task(this.task, this.taskvalue, this.colorval);
 }

f881a55d69f7133c2df6bc7900fb917021636ba7
steph-en-m <mstevejean@gmail.com>
2020-08-06 10:16:20 +0300

    move dropdown menu to appbar

--- store_ui/lib/main.dart
+++ store_ui/lib/main.dart
@@ -41,12 +41,44 @@ class DropdownScreenState extends State<DropdownScreen> {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          backgroundColor: Colors.green[900],
+          backgroundColor: Colors.green[400],
           title: Center(
               child: Text(
             'My Store',
             style: TextStyle(color: Colors.white),
           )),
+          actions: <Widget>[
+            DropdownButton<Item>(
+              hint: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              value: selectedMenu,
+              onChanged: (Item value) {
+                setState(() {
+                  selectedMenu = value;
+                });
+              },
+              items: menu.map((Item menu) {
+                return DropdownMenuItem<Item>(
+                  value: menu,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      menu.icon,
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        menu.name,
+                        style: TextStyle(color: Colors.deepPurple[500]),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
         ),
         body: (Padding(
             padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
@@ -63,35 +95,6 @@ class DropdownScreenState extends State<DropdownScreen> {
                             color: Colors.deepPurple[600],
                             letterSpacing: 2.0,
                           ))),
-
-                  DropdownButton<Item>(
-                    hint: Text("Menu",
-                        style: TextStyle(color: Colors.deepPurple[500])),
-                    value: selectedMenu,
-                    onChanged: (Item value) {
-                      setState(() {
-                        selectedMenu = value;
-                      });
-                    },
-                    items: menu.map((Item menu) {
-                      return DropdownMenuItem<Item>(
-                        value: menu,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            menu.icon,
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              menu.name,
-                              style: TextStyle(color: Colors.deepPurple[500]),
-                            ),
-                          ],
-                        ),
-                      );
-                    }).toList(),
-                  ),
 
                   SizedBox(
                     height: 18.0,
@@ -132,3 +135,109 @@ class DropdownScreenState extends State<DropdownScreen> {
     );
   }
 }
+
+/*
+// This app is a stateful, it tracks the user's current choice.
+class BasicAppBarSample extends StatefulWidget {
+  @override
+  _BasicAppBarSampleState createState() => _BasicAppBarSampleState();
+}
+
+class _BasicAppBarSampleState extends State<BasicAppBarSample> {
+  Choice _selectedChoice = choices[0]; // The app's "state".
+
+  void _select(Choice choice) {
+    // Causes the app to rebuild with the new _selectedChoice.
+    setState(() {
+      _selectedChoice = choice;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Basic AppBar'),
+          actions: <Widget>[
+            // action button
+            IconButton(
+              icon: Icon(choices[0].icon),
+              onPressed: () {
+                _select(choices[0]);
+              },
+            ),
+            // action button
+            IconButton(
+              icon: Icon(choices[1].icon),
+              onPressed: () {
+                _select(choices[1]);
+              },
+            ),
+            // overflow menu
+            PopupMenuButton<Choice>(
+              onSelected: _select,
+              itemBuilder: (BuildContext context) {
+                return choices.skip(2).map((Choice choice) {
+                  return PopupMenuItem<Choice>(
+                    value: choice,
+                    child: Text(choice.title),
+                  );
+                }).toList();
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ChoiceCard(choice: _selectedChoice),
+        ),
+      ),
+    );
+  }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Car', icon: Icons.directions_car),
+  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
+  const Choice(title: 'Boat', icon: Icons.directions_boat),
+  const Choice(title: 'Bus', icon: Icons.directions_bus),
+  const Choice(title: 'Train', icon: Icons.directions_railway),
+  const Choice(title: 'Walk', icon: Icons.directions_walk),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.headline4;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(BasicAppBarSample());
+}
+*/

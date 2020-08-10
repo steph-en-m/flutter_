75ef3d8448da4e4b3231f640994fbc88004d58f8
steph-en-m <mstevejean@gmail.com>
2020-08-10 10:14:55 +0300

    add placeholder widgets to test routes

--- store_ui/lib/screens/home.dart
+++ store_ui/lib/screens/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,19 +9,34 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, '/');
-            },
-            icon: Icon(Icons.show_chart),
-            label: Text('Analytics'),
-          ),
-        ],
-      ),
-    ));
+    return MaterialApp(
+        home: DefaultTabController(
+            length: 2,
+            child: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Color(0xff1976d2),
+                  bottom: TabBar(
+                    indicatorColor: Color(0xff9962D0),
+                    tabs: [
+                      Tab(icon: Icon(FontAwesomeIcons.home)),
+                      FlatButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/');
+                        },
+                        icon: Icon(Icons.show_chart, color: Colors.white70),
+                        label: Text(
+                          'Stats',
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                      ),
+                    ],
+                  ),
+                  title: Center(child: Text('The Awesome Store')),
+                ),
+                body: SafeArea(
+                  child: Column(
+                    children: <Widget>[],
+                  ),
+                ))));
   }
 }

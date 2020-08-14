9cd7049e6618a295d2781dea8c2899dc87d0e1b2
steph-en-m <mstevejean@gmail.com>
2020-08-14 16:54:02 +0300

    test different layouts

--- store_ui/lib/screens/home.dart
+++ store_ui/lib/screens/home.dart
@@ -13,30 +13,29 @@ class _HomeState extends State<Home> {
         home: DefaultTabController(
             length: 2,
             child: Scaffold(
-                appBar: AppBar(
-                  backgroundColor: Color(0xff1976d2),
-                  bottom: TabBar(
-                    indicatorColor: Color(0xff9962D0),
-                    tabs: [
-                      Tab(icon: Icon(FontAwesomeIcons.home)),
-                      FlatButton.icon(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/');
-                        },
-                        icon: Icon(Icons.show_chart, color: Colors.white70),
-                        label: Text(
-                          'Stats',
-                          style: TextStyle(color: Colors.white70),
-                        ),
+              appBar: AppBar(
+                backgroundColor: Color(0xff1976d2),
+                bottom: TabBar(
+                  indicatorColor: Color(0xff9962D0),
+                  tabs: [
+                    Tab(icon: Icon(FontAwesomeIcons.home)),
+                    FlatButton.icon(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      icon: Icon(Icons.show_chart, color: Colors.white70),
+                      label: Text(
+                        'Stats',
+                        style: TextStyle(color: Colors.white70),
                       ),
-                    ],
-                  ),
-                  title: Center(child: Text('The Awesome Store')),
+                    ),
+                  ],
                 ),
-                body: SafeArea(
-                  child: Column(
-                    children: <Widget>[],
-                  ),
-                ))));
+                title: Center(child: Text('The Awesome Store')),
+              ),
+              body: Column(
+                children: <Widget>[],
+              ),
+            )));
   }
 }
